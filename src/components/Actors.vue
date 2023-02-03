07b9363bebce70d5d393a84d90756422b1808775
MinnAK686 <anastasioak327@gmail.com>
2023-02-03 16:15:50 +0630

    Make actors pagination

--- src/components/Actors.vue
+++ src/components/Actors.vue
@@ -7,10 +7,29 @@ import { ref } from "vue";
 
 let moviesStore = useMoviesStore();
 let actors = ref([]);
-moviesStore.fetchPopularPeople(1).then((people) => {
+let page = ref(1);
+
+moviesStore.fetchPopularPeople(page.value).then((people) => {
     actors.value = people;
     // console.log(people);
 });
+
+let next = (pg) => {
+    page.value += pg;
+    moviesStore.fetchPopularPeople(page.value).then((people) => {
+        actors.value = people;
+    });
+};
+let prev = (pg) => {
+    page.value -= pg;
+    if (page.value > 0) {
+        moviesStore.fetchPopularPeople(page.value).then((people) => {
+            actors.value = people;
+        });
+    } else {
+        page.value = 1;
+    }
+};
 </script>
 
 <template>
@@ -22,6 +41,16 @@ moviesStore.fetchPopularPeople(1).then((people) => {
                 class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 gap-3"
             >
                 <Actor v-for="actor in actors" :key="actor.id" :actor="actor" />
+            </div>
+            <div class="mt-5 mb-3">
+                <div class="flex justify-center space-x-4">
+                    <a href="#" class="text-lg p-1" @click.prevent="prev(1)"
+                        >Previous</a
+                    >
+                    <a href="#" class="text-lg p-1" @click.prevent="next(1)"
+                        >Next</a
+                    >
+                </div>
             </div>
         </div>
         <div v-else>
